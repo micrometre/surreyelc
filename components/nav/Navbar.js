115250f401808a36046675b3f3567078455ec477
Henok <henokmicrometre@gmail.com>
2022-02-19 11:21:28 +0000

    styling components/

--- components/nav/Navbar.js
+++ components/nav/Navbar.js
@@ -6,15 +6,29 @@ import Social from '../../components/nav/NavbarSocial'
 import Paths from '../../components/nav/NavbarPaths'
 import Hero from '../../components/Hero.js'
 import Styles from './Navbar.module.css'
-import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap'
+import {Navbar, Container, Nav, NavDropdown, Offcanvas } from 'react-bootstrap'
 export const Navigation = () => {
   return (
     <>
       <Navbar bg="light" expand="lg" fixed="top" className={Styles.NavBarStyles}>
-          <Navbar.Toggle aria-controls="navbar-nav" />
-            <Navbar.Collapse id="navbar-nav" className={Styles}>
-                <Paths />
-          </Navbar.Collapse>
+        <Container fluid>
+          <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+    <Navbar.Toggle aria-controls="offcanvasNavbar" />
+    <Navbar.Offcanvas
+      id="offcanvasNavbar"
+      aria-labelledby="offcanvasNavbarLabel"
+      placement="end"
+    >
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+                                <Paths />
+                                <Phones />
+                                <Social />
+      </Offcanvas.Body>
+    </Navbar.Offcanvas>
+  </Container>
       </Navbar>
     </>
   );
