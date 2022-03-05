bb01e5c22943b2fd671c5c968dd4b4ca0eab2e85
Henok <henokmicrometre@gmail.com>
2022-03-05 15:02:45 +0000

    sortin offcanvas

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -10,52 +10,51 @@ function NavBarPhones( ) {
  return (
 <>
 
-  <Navbar bg="light" expand="lg" sticky="top" className="pathsList">
-    <Container fluid>
-            <Navbar.Toggle aria-controls="offcanvasNavbar" />
-   {data.map((e, i) => {
-     return (
-      <div className="pathsDiv"key={i}>
-       {e.contact.map((e, i) => {
-         return (
-        <a className="pathsLinks"href={e.href}key={i}title={e.title}>
-          {e.name}
-           </a>
-         );
-        })}
-      </div>
-     );
-      })}
+<Navbar bg="light" expand="lg" sticky="top" className="pathsList">
+<Container fluid>
+<Navbar.Toggle aria-controls="basic-navbar-nav" />
+<Navbar.Collapse id="basic-navbar-nav">
+{data.map((e, i) => {
+return (
+<div className="pathsDiv"key={i}>
+{e.contact.map((e, i) => {
+return (
+<a className="pathsLinks"href={e.href}key={i}title={e.title}>
+{e.name}
+</a>
+);
+})}
+</div>
+);
+})}
+</Navbar.Collapse>
 </Container >
-  </Navbar>
-  <Navbar bg="light" expand={false}>
-  <Container fluid>
-    <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-      <Navbar.Toggle aria-controls="offcanvasNavbar" />
-      <Navbar.Offcanvas
-      id="offcanvasNavbar"
-      aria-labelledby="offcanvasNavbarLabel"
-      placement="end"
-    >
-      <Offcanvas.Header closeButton>
-        <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
-      </Offcanvas.Header>
-      <Offcanvas.Body>
-        <Nav className="justify-content-end flex-grow-1 pe-3">
-          <Nav.Link href="#action1">Home</Nav.Link>
-          <Nav.Link href="#action2">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action5">
-              Something else here
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Offcanvas.Body>
-    </Navbar.Offcanvas>
-  </Container>
+</Navbar>
+<Navbar bg="light" expand="lg">
+<Container fluid>
+<Navbar.Toggle aria-controls="offcanvasNavbar" />
+<Navbar.Offcanvas
+  id="offcanvasNavbar"
+  aria-labelledby="offcanvasNavbarLabel" placement="start">
+<Offcanvas.Header closeButton>
+<Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+</Offcanvas.Header>
+<Offcanvas.Body>
+<Nav className="justify-content-end flex-grow-1 pe-3">
+<Nav.Link href="#action1">Home</Nav.Link>
+<Nav.Link href="#action2">Link</Nav.Link>
+<NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
+<NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+<NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+<NavDropdown.Divider />
+<NavDropdown.Item href="#action5">
+  Something else here
+</NavDropdown.Item>
+</NavDropdown>
+</Nav>
+</Offcanvas.Body>
+</Navbar.Offcanvas>
+</Container>
 </Navbar>
 <style jsx>{`
 .pathsLinks{
