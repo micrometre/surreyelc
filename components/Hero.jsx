f57b34a6c497713ada78416a4b2ddb510d119a8e
Henok <henokmicrometre@gmail.com>
2022-03-09 00:41:09 +0000

    sorting nav

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,55 +2,65 @@ import Image from 'next/image'
 import {Col, Row, Container } from 'react-bootstrap'
 
 function RiverAccessHeader ( ) {
-  const src = `/img/bathroom2.jpg`;
+  const src = `/img/logo.png`;
   return (
 <div className="container">
-
   <main>
-
     <section className="card-list">
-
       <article className="card">
-
+          <div  className="card">
+            <Image
+            src={src}
+            alt="About picture"
+            width={300}
+            height={300}
+          />
+          </div>
+        <p className="description">
+          Work carried out to the highest of standards.
+        </p>
       </article>
-
         <article className="card">
 
-        </article>
+          <div  className="card">
+            <Image
+            src={src}
+            alt="About picture"
+            width={300}
+            height={300}
+          />
+          </div>
+        <p className="description">
+          Work carried out to the highest of standards.
+        </p>
 
+
+
+        </article>
           <article className="card">
 
+          <div  className="card">
+            <Image
+            src={src}
+            alt="About picture"
+            width={300}
+            height={300}
+          />
+          </div>
+        <p className="description">
+          Work carried out to the highest of standards.
+        </p>
           </article>
-
     </section>
-
   </main>
 <style jsx>{`
 main {
-background: yellow;
-flex: 1;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-margin-bottom: 14rem;
 }
 .card-list {
-display: flex;
-flex-direction: row;
-margin: 3rem;
 }
 .card {
-margin: 1rem;
-flex-basis: 45%;
-padding: 2.5rem;
-text-align: left;
-color: inherit;
-text-decoration: none;
-border: 1px solid #eaeaea;
-border-radius: 10px;
-transition: color 0.15s ease, border-color 0.15s ease;
-background: #17141d;
+}
+.description{
 }
 }
       `}</style>
