82087df7fdb0a68a9e306477aa5d357e5dd3f5f8
Henok <henokmicrometre@gmail.com>
2022-03-09 23:39:43 +0000

    sorting navs

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,130 +1,59 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 function RiverAccessHeader ( ) {
     const src = `/img/bathroom2.jpg`;
 
   return (
-    <div className="container">
-
-      <main>
-
-
-
-            <Image
-            src="/img/map.webp"
-            alt="About picture"
-            width={500}
-            height={300}
-          />
-        <h1 className="title">
-         Areas we cover
-        </h1>
-
-        <p className="description">
-          Esher, Walton on Thames, Weybridge, Claygate,
-                        East Molesey, Cobham, Hersham, Oxshott, Thames Ditton, Long Ditton,
-                        Stoke D’Abernon, Hinchley Wood, Downside,
-                        St Georges Hill, Weston Green, Whiteley Village, Byfleet, West End and Oatlands.
-                    </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-
-            <Image
-            src="/img/map.webp"
-            alt="About picture"
-            width={500}
-            height={300}
-          />
-
-
-
+<div className="container">
+<main>
+<Image
+src="/img/map.jpeg"
+alt="About picture"
+width={1200}
+height={500}
+/>
+        <Link href="https://goo.gl/maps/HNW7FNaFFqYdAGXv5">
+          <a target="_blank">
+<h1 className="title">
+ Areas we cover
+</h1>
           </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+        </Link>
+  <p className="description">
+  Esher, Walton on Thames, Weybridge, Claygate,
+                East Molesey, Cobham, Hersham, Oxshott, Thames Ditton, Long Ditton,
+                Stoke D’Abernon, Hinchley Wood, Downside,
+                St Georges Hill, Weston Green, Whiteley Village, Byfleet, West End and Oatlands.
             </p>
-          </a>
-        </div>
-      </main>
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+</main>
+<style jsx>{`
+  .container {
+    min-height: 100vh;
+    padding: 0 0.5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  main {
+    padding: 5rem 0;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
         }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
+        .title  {
           color: #0070f3;
           text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
           text-decoration: underline;
         }
-
         .title {
           margin: 0;
           line-height: 1.15;
-          font-size: 4rem;
+          font-size: 3rem;
         }
 
         .title,
@@ -134,87 +63,10 @@ function RiverAccessHeader ( ) {
 
         .description {
           line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
+          font-size: 1.0rem;
         }
       `}</style>
     </div>
-
-
  );
 };
 export default RiverAccessHeader;
