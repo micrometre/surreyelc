120952a7ff52da58bbac4d4d61c54453415155c1
Henok <henokmicrometre@gmail.com>
2022-03-10 19:51:05 +0000

    sorting next.config.js

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,21 +1,7 @@
-import Head from 'next/head'
-import styles from './layout.module.css'
-
-
-export const siteTitle = 'Elmbridge Painting and Decorating Services Ltd'
 
 export default function Layout({ children}) {
   return (
     <div className="section">
-      <Head>
-        <link rel="icon" href="/img/logo.png" />
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="description" content="Professional Painters and Decorators in Elmbridge and surrounding areas." />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="Elmbridge Painting and Decorating Services Ltd" content={siteTitle} />
-            <title>{siteTitle}</title>
-      </Head>
       <main>{children}</main>
     </div>
   )
--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+
+const securityHeaders = [
+{
+  key: 'X-DNS-Prefetch-Control',
+  value: 'on'
+},
+]
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
+}
