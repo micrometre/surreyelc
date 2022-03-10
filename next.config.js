050413fa07b6cf2433baed7ae4b56cbab649f2d1
Henok <henokmicrometre@gmail.com>
2022-03-10 20:37:40 +0000

    csp policy

--- next.config.js
+++ next.config.js
@@ -2,12 +2,47 @@ module.exports = {
   reactStrictMode: true,
 }
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';
+`
+
 
 const securityHeaders = [
 {
   key: 'X-DNS-Prefetch-Control',
   value: 'on'
 },
+
+{
+  key: 'X-XSS-Protection',
+  value: '1; mode=block'
+},
+
+{
+  key: 'X-Frame-Options',
+  value: 'SAMEORIGIN'
+},
+
+
+{
+  key: 'Permissions-Policy',
+  value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+},
+
+    {
+  key: 'X-Content-Type-Options',
+  value: 'nosniff'
+},
+
+{
+  key: 'Referrer-Policy',
+  value: 'origin-when-cross-origin'
+},
+
 ]
 
 module.exports = {
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+              <Head>
+          {/* Styled-JSX will add this `nonce` to style tags on Client Side Rendering */}
+          {/* https://github.com/vercel/styled-jsx/blob/master/src/lib/stylesheet.js#L31 */}
+          {/* https://github.com/vercel/styled-jsx/blob/master/src/lib/stylesheet.js#L240 */}
+        </Head>
       <body>
         <Main />
         <NextScript />
