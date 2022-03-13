ab87449425e5feb6c67bc1e4a1caf4c9fb996dc7
Henok <henokmicrometre@gmail.com>
2022-03-13 09:24:27 +0000

    sorted css

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,4 +1,3 @@
-
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,12 +6,18 @@ function CopyRight() {
     <div className="river-container">
       <main>
         <div className="river-title">
-          <h1 className="river-header">
-            Interior
-Decoration
-          </h1>
+          <h1 className="river-header">Interior Decoration</h1>
         </div>
       </main>
+      <div className="description">
+        <p>
+          We cover all aspects of interior decorating from single rooms to
+          mansions. This includes restoration of woodwork, wooden windows and
+          any re-siliconing that is required around your property. Whether it be
+          living areas, kitchens, bedrooms, bathrooms, communal areas or garages
+          we will provide the perfect finish to your dream home or project.
+        </p>
+      </div>
       <style jsx>{`
         .river-container {
           min-height: 64vh;
@@ -40,13 +45,30 @@ Decoration
           background-size: cover;
           position: relative;
           width: 100%;
+          min-height: 60vh;
         }
-        .river-title, river-description {
+        .river-title,
+        river-description {
           margin: 1rem;
           color: white;
           font-weight: 300;
           font-size: 1.5rem;
         }
+        .description{
+          padding: 1rem;
+          margin: 2rem;
+        }
+
+        p {
+          text-align: left;
+          line-height: 1.7;
+          font-size: 1.09rem;
+          margin: .5rem;
+          flex-basis: 45%;
+          padding: 0.5rem;
+          text-decoration: none;
+        }
+
       `}</style>
     </div>
   );
