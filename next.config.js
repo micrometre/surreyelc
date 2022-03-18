585b339bfdb9706722314de284cc15f354eed140
Henok <henokmicrometre@gmail.com>
2022-03-18 18:37:40 +0000

    sorting content

--- next.config.js
+++ next.config.js
@@ -1,59 +1,62 @@
 module.exports = {
   reactStrictMode: true,
-}
+};
 
 const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self';
+  object-src 'none';
   child-src example.com;
   style-src 'self' example.com;
   font-src 'self';
-`
-
+`;
 
 const securityHeaders = [
-{
-  key: 'X-DNS-Prefetch-Control',
-  value: 'on'
-},
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "geolocation=*", // allow specified policies here
+  },
 
-{
-  key: 'X-XSS-Protection',
-  value: '1; mode=block'
-},
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
 
-{
-  key: 'X-Frame-Options',
-  value: 'SAMEORIGIN'
-},
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "Server",
+    value: "Apache", // phony server value
+  },
 
-
-{
-  key: 'Permissions-Policy',
-  value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
-},
-
-    {
-  key: 'X-Content-Type-Options',
-  value: 'nosniff'
-},
-
-{
-  key: 'Referrer-Policy',
-  value: 'origin-when-cross-origin'
-},
-
-]
+];
 
 module.exports = {
   async headers() {
     return [
       {
         // Apply these headers to all routes in your application.
-        source: '/:path*',
+        source: "/:path*",
         headers: securityHeaders,
       },
-    ]
+    ];
   },
-}
-
+};
