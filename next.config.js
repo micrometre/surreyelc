492bc58b63b0485b438c91ab4b88cfa45ee03c71
Henok <henokmicrometre@gmail.com>
2022-03-20 10:12:55 +0000

    pwa branch

--- next.config.js
+++ next.config.js
@@ -60,3 +60,18 @@ module.exports = {
     ];
   },
 };
+
+
+
+
+
+
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+})
