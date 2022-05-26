4a89c8ed17337f86fda26cbbf12082009c984df4
Henok <henokmicrometre@gmail.com>
2022-05-26 02:07:41 +0100

    correcting commercial

--- components/CommercialHero/CommercialHero.jsx
+++ components/CommercialHero/CommercialHero.jsx
@@ -2,35 +2,38 @@ import Image from 'next/image';
 import {
   useViewportScroll,
   motion,
-  useTransform,
+  useInvertedScale,
 } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
 import styles from "./CommercialHero.module.css";
+
+
 
 export function CommercialHero() {
   return (
-  <div className={styles.commercialContainer}>
-      <div className={styles.main}>
-        <div className={styles.row}>
-          <div className={styles.column}>
+    <motion.div>
+      <div className={styles.commercialContainer}>
+        <div className={styles.main}>
+          <div className={styles.row}>
+            <div className={styles.column}>
               <Image
                 src="/images/9.webp"
                 alt="About picture"
                 width={500}
                 height={300}
               />
-          </div>
-          <div className={styles.column}>
+            </div>
+            <div className={styles.column}>
               <Image
                 src="/images/10.webp"
                 alt="About picture"
                 width={500}
                 height={300}
               />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
